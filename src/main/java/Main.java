c450e39bbe49a45a329677499639dbde789467e6
Dmitry Popov <PopovDN93@gmail.com>
2023-05-22 17:51:41 +0700

    Fix Branch user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
-        System.out.println("branch user");
+        System.out.println("Branch user");
+        System.out.println("Fix Branch user");
     }
 }
