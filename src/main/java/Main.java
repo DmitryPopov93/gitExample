b3c18a4cd7ba61096a14b0e1c4b0e69e63c1850d
DmitryPopov93 <112748761+DmitryPopov93@users.noreply.github.com>
2023-05-22 17:44:42 +0700

    Update Main.java 17:43

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
-        System.out.println("Merge exBranch");
-        System.out.println("Merge exBranch2");
-        System.out.println("");
+        System.out.println("Branch master");
     }
 }
