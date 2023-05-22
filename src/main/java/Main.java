9cbb2a46c25b632c544d29c496e9b8c75a27eb8c
Dmitry Popov <PopovDN93@gmail.com>
2023-05-22 19:09:08 +0700

    rebase commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Branch master");
+
+        System.out.println("commit user2 - 1");
         System.out.println("commit user1 - 1");
     }
 }
