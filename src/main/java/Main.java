e805d19366196a82bc4c0d6459afd52bb1db5e61
Dmitry Popov <PopovDN93@gmail.com>
2023-05-22 17:49:58 +0700

    create branch user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
-        System.out.println("Merge exBranch");
-        System.out.println("Merge exBranch2");
-        System.out.println("");
+        System.out.println("branch user");
     }
 }
